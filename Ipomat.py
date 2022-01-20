54dfcbc9c46e1029ca06ff303f3cdb076b2cdeec
Retr0mous <72267897+DDizzzy79@users.noreply.github.com>
2022-01-20 14:27:10 +0800

    :zap:  A bug fixed

--- Ipomat.py
+++ Ipomat.py
@@ -1,37 +1,31 @@
 import argparse
 
 def Get_Args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-a","--ip",help="Your ip addr")
-    args = parser.parse_args()
-    return args
-def setup():
-    print(
-        '''
+    parser = argparse.ArgumentParser(
+        usage= '''
     __     ______   ______     __    __     ______     ______  
     /\ \   /\  == \ /\  __ \   /\ "-./  \   /\  __ \   /\__  _\ 
     \ \ \  \ \  _-/ \ \ \/\ \  \ \ \-./\ \  \ \  __ \  \/_/\ \/ 
     \ \_\  \ \_\    \ \_____\  \ \_\ \ \_\  \ \_\ \_\    \ \_\ 
     \/_/   \/_/     \/_____/   \/_/  \/_/   \/_/\/_/     \/_/ 
                                                             
-    usage: main.py [-h] [-u URL] [-a IP]
-
-    optional arguments:
-        -h, --help         show this help message and exit
-        -a IP, --ip IP     Your ip addr
-        '''            
+        '''         
     )
-    global args
-    if args.ip == None:
-        raise ValueError("You must input a Value of IP ")
-
+    parser.add_argument("ip",help="Your ip addr")
+    args = parser.parse_args()
+    return args
 if __name__ == "__main__":  
     args = Get_Args()
     sum = 0
     ip = args.ip
     list = list(ip.split('.'))
-    setup()
+    if len(list) != 4:
+        raise ValueError("Value of IP is wrong") #check
+    
     for i in range(len(list)):
+        if int(list[i]) > 256:
+            raise ValueError("The {}th digit of ip is wrong".format(int(i)+1))
+            
         sum += int(list[i])* pow(256,int(3-i))
     print(
         '''
